afa77ffc4f44a3c722f983753686722a90fbc8d1
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-02-07 20:12:15 +0000

    Basic script to create a canavas selector variable and for retrieving the '2d' object and its methods via .getContext()

---
+++ vanilla_canvas/script.js
@@ -0,0 +1,4 @@
+// Initialize drawing environment aka Canvas. 
+// cx receives a context object containing methods to interface drawing
+const canvas = document.querySelector("canvas");
+const cx = canvas.getContext("2d");
