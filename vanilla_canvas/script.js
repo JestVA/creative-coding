6a77e48b25034a03feed71eaef098f3ba7a2ebcd
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-02-07 20:48:32 +0000

    Add styling to canvas element (border) as well as a style for the page (import a font, display flex, centered content)

--- vanilla_canvas/script.js
+++ vanilla_canvas/script.js
@@ -1,4 +1,16 @@
 // Initialize drawing environment aka Canvas. 
 // cx receives a context object containing methods to interface drawing
+
 const canvas = document.querySelector("canvas");
 const cx = canvas.getContext("2d");
+
+// General setting (width, height)
+
+const dimensions = [500, 500];
+
+// Create attributes and set the size from the dimensions array
+
+const canvas_width = canvas.setAttribute("width", dimensions[0]);
+const canvas_height = canvas.setAttribute("height", dimensions[1]);
+
+
