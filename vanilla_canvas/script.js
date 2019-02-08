3ce655e808d8684cd52c3c73023de013d8da7e4c
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-02-08 23:11:38 +0000

    a rectangle

--- vanilla_canvas/script.js
+++ vanilla_canvas/script.js
@@ -1,16 +1,16 @@
 // Initialize drawing environment aka Canvas. 
 // cx receives a context object containing methods to interface drawing
-
 const canvas = document.querySelector("canvas");
 const cx = canvas.getContext("2d");
 
 // General setting (width, height)
-
 const dimensions = [500, 500];
 
 // Create attributes and set the size from the dimensions array
-
 const canvas_width = canvas.setAttribute("width", dimensions[0]);
 const canvas_height = canvas.setAttribute("height", dimensions[1]);
 
+// Start drawing 
+cx.fillStyle = "orange";
+cx.fillRect(10, 10, 100, 50)
 
