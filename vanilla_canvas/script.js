a962d9bae7b219a752b9524ee3d0feba8ddda59f
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-02-10 13:51:37 +0000

    add paths with both stroke and fill methods

--- vanilla_canvas/script.js
+++ vanilla_canvas/script.js
@@ -11,6 +11,59 @@ const canvas_width = canvas.setAttribute("width", dimensions[0]);
 const canvas_height = canvas.setAttribute("height", dimensions[1]);
 
 // Start drawing 
-cx.fillStyle = "orange";
-cx.fillRect(10, 10, 100, 50)
+// fillStyle is a property of the context object
 
+/*
+cx.fillStyle = "orange";
+*/
+
+// 100 pixels wide, 50 pixels high
+// top-left coordinates at (10, 10)
+// (0, 0) is always top-left corner
+// fillRect() is a method of the context object
+
+/*
+cx.fillRect(10, 10, 100, 50)
+*/
+
+// lines and surfaces
+
+/*
+cx.strokeStyle = "grey"
+*/
+
+// first coordinate is X-axis
+// second coordinate is Y-axis
+
+/*
+cx.strokeRect(50, 70, 50, 50);
+cx.lineWidth = 5;
+cx.strokeRect(200, 10, 50, 50);
+*/
+
+// paths (a sequence of lines)
+
+/*
+cx.beginPath();
+for (let y = 10; y < 500; y += 10) {
+    cx.strokeStyle = "orange"
+    cx.moveTo(10, y);
+    cx.lineTo(490, y);
+    cx.moveTo(60, y - 55)
+    cx.lineTo(440, y - 55)
+}
+cx.stroke();
+*/
+
+// same paths but with fill()
+/*
+cx.beginPath();
+cx.moveTo(50, 10);
+cx.lineTo(10, 70);
+cx.lineTo(90, 70)
+// if the path is not closed when the fill method is called
+// it will create a new lineTo from the last line to the first
+cx.fill()
+*/
+
+// curves
