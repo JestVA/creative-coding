31afd90af23dbec4b28c76de91a718aa32124f6d
Dorin <d.dumitrascuta@gmail.com>
2019-02-21 00:11:22 +0000

    how to keep track of state while also calling back the function in a recurssive manner?

--- vanilla_canvas/boilerplate/kollatz-conjecture.js
+++ vanilla_canvas/boilerplate/kollatz-conjecture.js
@@ -1,6 +1,7 @@
 // https://en.wikipedia.org/wiki/Collatz_conjecture
 
 const hotpo = n => {
+    let counter = 0
     let steps = 0;
     if (n === 0 || n === 1) {
       return steps = 0
@@ -9,26 +10,15 @@ const hotpo = n => {
     if (n % 2 === 0) {
         n /= 2
         steps += 1
-        hotpo(n)
+        n > 1 ? hotpo(n) : steps
     }
 
 
     return steps
-
-
-
-
-
-
-  
-  
-  
-  
-  
-  
-  // Append 1 to the (right) end of the number in binary (giving 2n + 1);
-  // Add this to the original number by binary addition (giving 2n + 1 + n = 3n + 1);
-  // Remove all trailing "0"s (i.e. repeatedly divide by two until the result is odd).
-  
-  
 }
+
+    /*
+  Append 1 to the (right) end of the number in binary (giving 2n + 1);
+  Add this to the original number by binary addition (giving 2n + 1 + n = 3n+1);
+  Remove all trailing "0"s (i.e. repeatedly divide by two until the result is odd).
+    */
