23f2be9bf95b294856b5ebf5d13ace7878997335
Dorin <d.dumitrascuta@gmail.com>
2020-05-27 01:11:13 +0100

    Add some utility getters and setters to the Canvas class

--- src/app.js
+++ src/app.js
@@ -1,17 +1,55 @@
 import Canvas from './lib/canvas';
-import palette from './lib/palette';
-
+import palettes from './lib/palettes';
+import { hex } from 'wcag-contrast';
 import seedrandom from 'seedrandom';
 import arraySuffle from 'array-shuffle';
-import { hex } from 'wcag-contrast';
 import SimplexNoise from 'simplex-noise';
 
+
 const myFirstCanvas = new Canvas(document.getElementById("generative"), 800, 800);
+
+const init = () => {
+	window._myFirstCanvas = myFirstCanvas;
+	window._palettes = palettes;
+};
+
+const bestContrast = (palette, bg) =>
+{
+	let bestContrast = 0,
+		cRatio = 0;
+
+	palette.forEach((colour, i) =>
+	{
+		if(hex(bg, colour) > cRatio)
+		{
+			bestContrast = i;
+			cRatio = hex(bg, colour);
+		}
+	});
+
+	return bestContrast;
+};
+
+const drawPalette = () =>
+{
+
+}
+
 
 
 console.log(myFirstCanvas);
 
+myFirstCanvas.canvasHeight
+myFirstCanvas.canvasWidth
 
+myFirstCanvas.newWidth = 5
+myFirstCanvas.newHeight = 700
+console.log(myFirstCanvas.context)
+
+
+console.log(myFirstCanvas)
+
+init();
 
 
 
--- src/lib/canvas.js
+++ src/lib/canvas.js
@@ -1,10 +1,32 @@
 class Canvas {
 	constructor(domElement, height, width)
 	{
-		domElement.setAttribute("width", width);
-		domElement.setAttribute("height", height);
-		return domElement.getContext("2d");
+		this.domElement = domElement;
+		this.domElement.setAttribute('height', height);
+		this.domElement.setAttribute('width', width);
+		this.context = this.domElement.getContext("2d");
 	}
+
+	get canvasHeight()
+	{
+		return this.context.canvas.height;
+	}
+
+	get canvasWidth()
+	{
+		return this.context.canvas.width;
+	}
+
+	set newWidth(newWidth)
+	{
+		this.domElement.setAttribute('width', newWidth)
+	}
+
+	set newHeight(newHeight)
+	{
+		this.domElement.setAttribute('height', newHeight);
+	}
+
 }
 
 export default Canvas;
